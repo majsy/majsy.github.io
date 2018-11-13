2d674e9b40aaeaf26a4f9a13e6d2bbeb858b4d45
majsy <madeleineauvinen@gmail.com>
2018-11-13 13:09:11 -0500

    first commit

---
+++ precache-manifest.fe20b53c6655446b6449fe0392a19bd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d817e1b793312f8873a7",
+    "url": "/static/js/main.d817e1b7.chunk.js"
+  },
+  {
+    "revision": "0bcb95da6701eab7e77d",
+    "url": "/static/js/1.0bcb95da.chunk.js"
+  },
+  {
+    "revision": "d817e1b793312f8873a7",
+    "url": "/static/css/main.ea6b5c35.chunk.css"
+  },
+  {
+    "revision": "605a47ec213369e1ecf859fbb186374c",
+    "url": "/index.html"
+  }
+];
